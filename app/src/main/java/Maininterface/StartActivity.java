fe5365f40d937766a1d934f23f8318a7a1bdf52e
DongWooE <63181439+DongWooE@users.noreply.github.com>
2020-11-28 05:10:05 +0900

    version1.2

--- app/src/main/java/Maininterface/StartActivity.java
+++ app/src/main/java/Maininterface/StartActivity.java
@@ -11,11 +11,14 @@ import com.captech.ar.R;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class StartActivity extends AppCompatActivity {
 
     Button btn_start;
+    Button btn_multi;
+    Button btn_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +26,28 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         btn_start = findViewById(R.id.btn_start);
+        btn_multi = findViewById(R.id.btn_multi);
+        btn_exit = findViewById(R.id.btn_exit);
+
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(StartActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(StartActivity.this, "지금은 준비중이예요", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
