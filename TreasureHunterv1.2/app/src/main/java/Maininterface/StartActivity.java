6b4159951961119b0b41deb305d267ab99217460
ckwogus37 <ckwogus37@gmail.com>
2020-11-29 19:24:32 +0900

    mainbgm plus

--- TreasureHunterv1.2/app/src/main/java/Maininterface/StartActivity.java
+++ TreasureHunterv1.2/app/src/main/java/Maininterface/StartActivity.java
@@ -1,5 +1,7 @@
 package Maininterface;
 
+import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,14 +19,29 @@ import android.widget.Toast;
 //어플의 가장 초기화면 by 이동우
 public class StartActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
     Button btn_start;
     Button btn_multi;
     Button btn_exit;
+
+    //액티비티가 종료될때 이 메소드를 실행함
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer=null;
+        }
+    }
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);    
+        setContentView(R.layout.activity_start);
+
+        mediaPlayer = MediaPlayer.create(StartActivity.this,R.raw.mainbgm);
+        mediaPlayer.start();
 
         //xml 파일의 버튼들과 연결
         btn_start = findViewById(R.id.btn_start);
@@ -36,8 +53,12 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //현재 액티비티에서 GameRuleActivity로 이동
-                Intent intent = new Intent(StartActivity.this, GameRuleActivity.class); 
-                startActivity(intent);  
+                Intent intent = new Intent(StartActivity.this, GameRuleActivity.class);
+                startActivity(intent);
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
                 finish();
             }
         });
@@ -55,8 +76,8 @@ public class StartActivity extends AppCompatActivity {
         btn_exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //액티비티 종료
-                finish();
+                ActivityCompat.finishAffinity(StartActivity.this);
+                System.exit(0); //어플리케이션 종료
             }
         });
 
